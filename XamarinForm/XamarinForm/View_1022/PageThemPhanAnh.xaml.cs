7a68ce57fd9e2aab86aec6246ce1465fcac026e2
dohoangphuong <hp.codoc@yahoo.com.vn>
2016-07-21 09:36:34 +0700

    +Add: Các file 1022. Lưu ý: Phiên bản hiện tại còn lỗi vui lòng khọng sử dụng

---
+++ XamarinForm/XamarinForm/View_1022/PageThemPhanAnh.xaml.cs
@@ -0,0 +1,159 @@
+﻿using Android.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinForm.View
+{
+    public partial class PageThemPhanAnh : ContentPage
+    {
+        Picker pickerDistrict;
+        Picker pickerTown;
+        public PageThemPhanAnh()
+        {
+
+            Label header = new Label
+            {
+                Text = "NỘI DUNG PHẢN ÁNH",
+                FontSize = 60,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+
+            Label lbEmail = new Label()
+            {
+                Text = "Nội dung phản ánh (*)",
+                FontSize = 20,
+            };
+            Editor editDeatail = new Editor
+            {
+                Keyboard = Keyboard.Text,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                FontSize = 20,
+            };
+           
+
+            Entry entDiaChi = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Nhập vị trí",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            Entry entDuong = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Nhập đường",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            pickerDistrict = new Picker
+            {
+                Title = "Vui lòng chọn quận",
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            pickerTown = new Picker
+            {
+                Title = "Vui lòng chọn phường",
+                VerticalOptions = LayoutOptions.Center,
+            };
+
+            Button btnTiepTuc = new Button
+            {
+                Text = "Tiếp tục",
+                //FontSize = 20,
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                //BorderWidth = 1,
+                //HorizontalOptions = LayoutOptions.Center,
+                //VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            btnTiepTuc.Clicked += btnTiepTucClick;
+            //chọn loại quận trong picker
+            pickerDistrict.SelectedIndexChanged += pickerDistrictSelectedIndexChanged;
+            //chọn loại phường trong picker
+            pickerTown.SelectedIndexChanged += pickerTownSelectedIndexChanged;
+
+            //get listDistrict
+            if (Constants.lstDistrict.Count > 0)
+            {
+                foreach (DM_QUAN itemlstDistrict in Constants.lstDistrict)
+                {
+                    pickerDistrict.Items.Add(itemlstDistrict.TenQuan);
+                }
+            }
+
+            
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    //--------------------
+                    //Màn hình nội dung phản ánh
+                    lbEmail,
+                    editDeatail,
+                    entDiaChi,
+                    entDuong,
+                    pickerDistrict,
+                    pickerTown,
+                    btnTiepTuc
+                }
+            };
+
+        }
+        async void btnTiepTucClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageThongTinNguoiDung());
+        }
+        
+        async void pickerDistrictSelectedIndexChanged(object sender, EventArgs args)
+        {
+            if (pickerDistrict.SelectedIndex == -1)
+            {
+                //entDistrict.Text = pickerDistrict.Items[0];
+            }
+            else
+            {
+                //entDistrict.Text = pickerDistrict.Items[pickerDistrict.SelectedIndex];
+            }
+
+            Constants.lstTown = null;
+            Constants.lstTown = Constants._TPhanAnhController.GetTown(Constants.lstDistrict[pickerDistrict.SelectedIndex].QuanID);
+
+            //get listTown
+
+            Log.Warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaa", Constants.lstTown.Count.ToString());
+            if (Constants.lstTown.Count > 0)
+            {
+                foreach (DM_PHUONG itemlstTown in Constants.lstTown)
+                {
+                    pickerDistrict.Items.Add(itemlstTown.TenPhuong);
+                }
+            }
+
+
+        }
+
+        async void pickerTownSelectedIndexChanged(object sender, EventArgs args)
+        {
+            if (pickerTown.SelectedIndex == -1)
+            {
+                //entDistrict.Text = pickerDistrict.Items[0];
+            }
+            else
+            {
+                //entDistrict.Text = pickerDistrict.Items[pickerDistrict.SelectedIndex];
+            }
+        }
+    }
+}
---
+++ XamarinForm/XamarinForm/View_1022/PageThongTinNguoiDung.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinForm.Views
+{
+    public partial class PageThongTinNguoiDung : ContentPage
+    {
+        Picker pickerDistrict;
+        Picker pickerTown;
+        public PageThongTinNguoiDung()
+        {
+            Label header = new Label
+            {
+                Text = "THÔNG TIN CÁ NHÂN",
+                FontSize = 60,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+
+            Label lbEmail = new Label()
+            {
+                Text = "Email (*)",
+                FontSize = 20,
+            };
+            Entry entEmail = new Entry
+            {
+                Keyboard = Keyboard.Email,
+                Placeholder = "Nhập Email",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            Entry entHoTen = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Nhập họ tên",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            Entry entDiaChi = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Nhập địa chỉ",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            Entry entDuong = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Nhập đường",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            Entry entQuan = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Vui lòng chọn quận ",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+            Entry entPhuong = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Vui lòng chọn phường",
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            pickerDistrict = new Picker
+            {
+                Title = "Vui lòng chọn quận",
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            pickerTown = new Picker
+            {
+                Title = "Vui lòng chọn phường",
+                VerticalOptions = LayoutOptions.Center,
+            };
+
+            Button btnTiepTuc = new Button
+            {
+                Text = "Tiếp tục",
+                FontSize = 20,
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            btnTiepTuc.Clicked += btnTiepTucClick;
+            //chọn loại quận trong picker
+            pickerDistrict.SelectedIndexChanged += pickerDistrictSelectedIndexChanged;
+            //chọn loại phường trong picker
+            pickerTown.SelectedIndexChanged += pickerTownSelectedIndexChanged;
+            //get listDistrict
+            if (Constants.lstDistrict.Count > 0)
+            {
+                foreach (DM_QUAN itemlstQuan in Constants.lstDistrict)
+                {
+                    pickerDistrict.Items.Add(itemlstQuan.TenQuan);
+                }
+            }
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    //--------------------
+                    //Màn hình nội dung phản ánh
+                    lbEmail,
+                    entEmail,
+                    entHoTen,
+                    entDiaChi,
+                    entDuong,
+                    pickerDistrict,
+                    pickerTown,
+                    btnTiepTuc
+                }
+            };
+        }
+        async void btnTiepTucClick(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new PageLayAnh());
+            //Navigation.InsertPageBefore(new PageLayAnh(), this);
+            //await Navigation.PopAsync();
+            await Navigation.PushAsync(new PageLayAnh());
+        }
+        async void pickerDistrictSelectedIndexChanged(object sender, EventArgs args)
+        {
+            if (pickerDistrict.SelectedIndex == -1)
+            {
+                pickerDistrict.SelectedIndex = 0;
+                //entDistrict.Text = pickerDistrict.Items[0];
+            }
+            else
+            {
+                //entDistrict.Text = pickerDistrict.Items[pickerDistrict.SelectedIndex];
+            }
+
+            Constants.lstTown = null;
+            //get list town is id of district
+            Constants.lstTown = Constants._TPhanAnhController.GetTown(Constants.lstDistrict[pickerDistrict.SelectedIndex].QuanID);
+        }
+
+        async void pickerTownSelectedIndexChanged(object sender, EventArgs args)
+        {
+            if (pickerTown.SelectedIndex == -1)
+            {
+                pickerTown.SelectedIndex = 0;
+                //entDistrict.Text = pickerDistrict.Items[0];
+            }
+            else
+            {
+                //entDistrict.Text = pickerDistrict.Items[pickerDistrict.SelectedIndex];
+            }
+        }
+    }
+}
