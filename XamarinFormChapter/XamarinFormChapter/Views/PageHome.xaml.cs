05d192a3637fbe838ae4ff1690ff544eb838dc47
dohoangphuong <hp.codoc@yahoo.com.vn>
2016-09-01 02:24:38 +0700

    *Update thêm search vị trí, các gói package

---
+++ XamarinFormChapter/XamarinFormChapter/Views/PageHome.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinFormChapter.Views
+{
+    public partial class PageHome : ContentPage
+    {
+        public PageHome()
+        {
+            Title = "Home Page";
+            Image imageBanner = new Image
+            {
+                Source = "banner.jpg",
+            };
+
+            Button btnAddReflect = new Button
+            {
+                Text = "THÊM PHẢN ÁNH",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 15,
+                BackgroundColor = Color.FromHex("#3297E9"),
+            };
+            btnAddReflect.Clicked += btnAddReflect_Clicked;
+
+            Button btnShowReflect=new Button
+            {
+                Text = "TRA CỨU PHẢN ÁNH",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 15,
+                BackgroundColor = Color.FromHex("#3297E9"),
+            };
+            btnShowReflect.Clicked += BtnShowReflect_Clicked;
+
+            Button btnReport = new Button
+            {
+                Text = "THỐNG KÊ",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 15,
+                BackgroundColor = Color.FromHex("#3297E9"),
+
+            };
+            btnReport.Clicked += BtnReport_Clicked;
+
+            Button btnHelp = new Button
+            {
+                Text = "GIÚP ĐỠ",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 15,
+                BackgroundColor = Color.FromHex("#3297E9"),
+            };
+            btnHelp.Clicked += BtnHelp_Clicked;
+
+            Button btnConfig = new Button
+            {
+                Text = "Cấu hình",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 15,
+                BackgroundColor = Color.FromHex("#3297E9"),
+            };
+            btnConfig.Clicked += BtnConfig_Clicked;
+
+            Grid grid = new Grid
+            {
+              //  Padding = new Thickness(10, 10, 10, 20),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions=LayoutOptions.CenterAndExpand,
+
+                RowDefinitions =//dòng
+                {
+                    new RowDefinition { Height = new GridLength(80, GridUnitType.Absolute) },
+                    new RowDefinition { Height = new GridLength(30, GridUnitType.Absolute) },
+                    new RowDefinition { Height = new GridLength(80, GridUnitType.Absolute) },
+                },
+                ColumnDefinitions =//cột
+                {
+                    new ColumnDefinition { Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition { Width = new GridLength(30, GridUnitType.Absolute) },
+                    new ColumnDefinition { Width = new GridLength(100, GridUnitType.Absolute) }
+                }
+            };
+
+            grid.Children.Add(btnAddReflect, 0,0);
+            grid.Children.Add(btnReport, 0, 2);
+            grid.Children.Add(btnShowReflect, 2, 0);
+            grid.Children.Add(btnHelp, 2, 2);
+            grid.Children.Add(btnConfig, 0, 4);
+            grid.Children.Add(new Label(), 0, 1);
+            grid.Children.Add(new Label(), 1, 0);
+            grid.Children.Add(new Label(), 1, 1);
+            grid.Children.Add(new Label(), 1, 2);
+            grid.Children.Add(new Label(), 2, 1);
+            // Accomodate iPhone status bar.
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    imageBanner,
+                    grid,
+                }
+            };
+        }
+
+        private void BtnConfig_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void BtnHelp_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private async void BtnReport_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void BtnShowReflect_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageListLinhVuc());
+        }
+
+        private async void btnAddReflect_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageThemPhanAnh());
+        }
+    }
+}
---
+++ XamarinFormChapter/XamarinFormChapter/Views/PageMaps.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using XamarinFormChapter.Models;
+
+namespace XamarinFormChapter.Views
+{
+    public class PageMaps : ContentPage
+    {
+        Double  dbLatitude =0, dbLong=0;
+        public PageMaps()
+        {
+            var customMap = new CustomMap
+            {
+                MapType = MapType.Street,
+                WidthRequest = App.ScreenWidth,
+                HeightRequest = App.ScreenHeight
+            };
+
+            Search(); 
+
+            var pin = new CustomPin
+            {
+                Pin = new Pin
+                {
+                    Type = PinType.Place,
+                    Position = new Position(dbLatitude, dbLong),
+                    Label = "Xamarin San Francisco Office",
+                    Address = "394 Pacific Ave, San Francisco CA"
+                },
+                Id = "Xamarin",
+                Url = "http://xamarin.com/about/"
+            };
+
+            customMap.CustomPins = new List<CustomPin> { pin };
+            customMap.Pins.Add(pin.Pin);
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(dbLatitude, dbLong), Distance.FromMiles(1.0)));
+
+           
+            //Giao diện
+            Content = customMap;
+
+
+        }
+
+        public async void Search()
+        {
+            Geocoder geoCoder;
+
+            geoCoder = new Geocoder();
+            string address = "Ha Noi";
+            var approximateLocations = await geoCoder.GetPositionsForAddressAsync(address);
+            foreach (var position in approximateLocations)
+            {
+                dbLatitude = position.Latitude;
+                dbLong = position.Longitude;
+               // geocodedOutputLabel.Text += position.Latitude + ", " + position.Longitude + "\n";
+            }
+        }
+    }
+}
